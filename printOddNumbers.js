fc699dc87384738fb28340b748290855b8975e77
priyankamoorthy264-svg <priyankamoorthy264@gmail.com>
2026-05-12 15:06:32 +0530

    Add files via upload

---
+++ printOddNumbers.js
@@ -0,0 +1,15 @@
+function printOddNumbers(num){
+
+ 
+for ( let r = 1 ; r<=num;r++) {
+       if (r%2!=0)
+     {
+        console.log("This is an odd number",r);
+        
+     }
+  
+    }
+  
+
+}
+printOddNumbers(25)
