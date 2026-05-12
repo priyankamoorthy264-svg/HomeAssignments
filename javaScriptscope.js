0651937d649d4bfbaa23c4874a3bb13237616deb
priyankamoorthy264-svg <priyankamoorthy264@gmail.com>
2026-05-12 13:50:20 +0530

    Add files via upload

---
+++ javaScriptscope.js
@@ -0,0 +1,14 @@
+var  genderType = "female"
+function printGender(){
+    let color = "brown"
+    if (genderType==="female"){
+        var age = 30
+         let color = "pink"
+         console.log(color);
+         
+    }
+    console.log(age);
+    console.log(color)
+    
+}
+printGender()
